0e860fe2e6401ec577d78257b1451ec66ca38039
Valkorion <82095453+Erwan923@users.noreply.github.com>
2022-11-25 16:49:42 +0100

    Add files via upload

---
+++ OfficeInstall.bat
@@ -0,0 +1,4 @@
+@echo off
+start C:\Users\ebillard\Desktop\Office2021\office_xa4ELZnG.exe
+cscript ospp.vbs /inpkey: DDVGM-B9NJK-7CH87-9MX3G-P7PC7
+pause
---
+++ VisioInstall.bat
@@ -0,0 +1,4 @@
+@echo off
+start C:\Users\ebillard\Desktop\Office2021\office_xa4ELZnG.exe
+cscript ospp.vbs /inpkey: QJBYF-NC7XY-M39PT-QRX7V-JQP9K
+pause
